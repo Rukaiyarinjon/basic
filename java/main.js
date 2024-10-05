ccc53628d772ef81903b5945900bc6c8fe7c2d39
unknown <info.rukaiyarinjon@gmail.com>
2024-10-05 13:59:53 +0600

    java

---
+++ java/main.js
@@ -0,0 +1,27 @@
+/**
+ * var
+ * let
+ * const
+ */
+
+// var number = 12;
+// console.log(number);
+
+// var number1 = 12 + 12;
+// console.log(number1);
+
+// var jhuri ="apple";
+// let box ="12"
+// const balti = "tissu";
+
+// box = 14;
+// console.log(box);
+
+// var a = 3;
+// var b = 5;
+// var sum = a+b;
+// console.log(sum);
+// var a = 3^2;
+// console.log(a);
+var a = 3^2;
+console.log(a);
